65bdba3c64efcf7c4feae32c96465b48a1734c1a
Bharat Kathi <bharat1031@gmail.com>
2023-11-28 00:28:45 -0800

    just some initial shit

---
+++ src/app/utils/consts.tsx
@@ -0,0 +1,5 @@
+export const SOCIAL_LINKS = {
+    instagram: 'https://instagram.com/gauchoracingucsb',
+    twitter: 'https://twitter.com/bk1031_official',
+    linkedin: 'https://www.linkedin.com/company/gaucho-racing-at-uc-santa-barbara',
+};
