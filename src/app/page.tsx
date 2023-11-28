f1811a95ddce83bb9dbc3ef892cafd70e42f5f04
Bharat Kathi <bharat1031@gmail.com>
2023-11-28 14:59:20 -0800

    just tryin some shit, added top navbar and about text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,26 @@ import { useEffect, useState } from "react";
 export default function Home() {
 
   const [calcWidth, setCalcWidth] = useState(0);
+  const [showNavbar, setShowNavbar] = useState(false);
 
   useEffect(() => {
+    handleScroll();
     handleResize();
+    window.addEventListener("scroll", handleScroll);
     window.addEventListener("resize", handleResize);
     return () => {
+      window.removeEventListener("scroll", handleScroll);
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setShowNavbar(true);
+    } else {
+      setShowNavbar(false);
+    }
+  };
 
   const handleResize = () => {
     const newWindowDimensions = {
@@ -38,6 +50,41 @@ export default function Home() {
 
   return (
     <main className=''>
+      {showNavbar ? <nav className='bg-black h-32 w-full flex justify-center items-center fixed top-0 z-10'>
+        <div className='flex items-center mt-3 mb-3'>
+          <button className="relative inline-flex items-center justify-center mt-2 p-0.5 mb-2 me-2 overflow-hidden text-md font-light text-white rounded-lg group hover:bg-gradient-to-br from-gr-purple to-gr-pink">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black rounded-md">
+              ABOUT
+            </span>
+          </button>
+          <button className="relative inline-flex items-center justify-center mt-2 p-0.5 mb-2 me-2 overflow-hidden text-md font-light text-gray-400 hover:text-white rounded-lg group hover:bg-gradient-to-br from-gr-purple to-gr-pink">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black rounded-md">
+              TEAM
+            </span>
+          </button>
+          <button className="relative inline-flex items-center justify-center mt-2 p-0.5 mb-2 me-2 overflow-hidden text-md font-light text-gray-400 hover:text-white rounded-lg group hover:bg-gradient-to-br from-gr-purple to-gr-pink">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black rounded-md">
+              CARS
+            </span>
+          </button>
+          <img src='logo/mechanic-logo.png' alt='Logo' className='h-24 ml-4 mr-4' />
+          <button className="relative inline-flex items-center justify-center mt-2 p-0.5 mb-2 me-2 overflow-hidden text-md font-light text-gray-400 hover:text-white rounded-lg group hover:bg-gradient-to-br from-gr-purple to-gr-pink">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black rounded-md">
+              SPONSORS
+            </span>
+          </button>
+          <button className="relative inline-flex items-center justify-center mt-2 p-0.5 mb-2 me-2 overflow-hidden text-md font-light text-gray-400 hover:text-white rounded-lg group hover:bg-gradient-to-br from-gr-purple to-gr-pink">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black rounded-md">
+              NEWS
+            </span>
+          </button>
+          <button className="relative inline-flex items-center justify-center mt-2 p-0.5 mb-2 me-2 overflow-hidden text-md font-light text-gray-400 hover:text-white rounded-lg group hover:bg-gradient-to-br from-gr-purple to-gr-pink">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black rounded-md">
+              CONTACT
+            </span>
+          </button>
+        </div>
+      </nav> : <div></div>}
       <div className=''>
         <ReactPlayer 
           playing
@@ -58,7 +105,7 @@ export default function Home() {
           }}
         />
       </div>
-      <div className='bg-purple-900 bg-opacity-70 flex justify-center items-center h-screen'>
+      <div className='bg-gr-purple bg-opacity-50 flex justify-center items-center h-screen'>
         <div className='text-white flex flex-col items-center justify-center'>
           <h1 className='text-5xl font-bold p-4'>
             Gaucho Racing
@@ -77,6 +124,37 @@ export default function Home() {
               <LinkedinIcon className='h-8 w-8 ml-4 mr-4' />
             </Link>
           </div>
+        </div>
+      </div>
+      <div className='' style={{
+        backgroundImage: "url('home/about.jpeg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}>
+        <div className='bg-black bg-opacity-60 p-64'>
+          <div className='text-white flex flex-col items-center justify-center'>
+            <h1 className='text-5xl font-medium p-8'>
+              Our Story
+            </h1>
+            <h3 className='text-xl font-thin'>
+              Gaucho Racing is the continuation of a long history of motorsports in Santa Barbara.
+              Though teams have come and gone, it is our goal to create something that is lasting and will persevere through the years to highlight the engineering and business prowess of UC Santa Barbara Students.
+              We put a strong focus on educating new members, providing them with the tools they need to succeed, both in school and outside.
+            </h3>
+          </div>
+        </div>
+      </div>
+      <div className='bg-black bg-opacity-50 p-64'>
+        <div className='text-white flex flex-col items-center justify-center'>
+          <h1 className='text-4xl font-bold p-8'>
+            Our Story
+          </h1>
+          <h3 className='text-xl font-light'>
+            Gaucho Racing is the continuation of a long history of motorsports in Santa Barbara.
+            Though teams have come and gone, it is our goal to create something that is lasting and will persevere through the years to highlight the engineering and business prowess of UC Santa Barbara Students.
+            We put a strong focus on educating new members, providing them with the tools they need to succeed, both in school and outside.
+          </h3>
         </div>
       </div>
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
 
 const config: Config = {
   content: [
@@ -12,6 +13,10 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'gr-pink': '#e105a3',
+        'gr-purple': '#8412fc',
       },
     },
   },
