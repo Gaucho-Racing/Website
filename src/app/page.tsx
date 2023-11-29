4c745590c6b5aca373eb072c68d394e93c1c2dca
Bharat Kathi <bharat1031@gmail.com>
2023-11-29 11:42:40 -0800

    added header animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,7 +59,18 @@ export default function HomePage() {
 
   return (
     <main className=''>
-      {scrollY > windowHeight - 132 ? <nav className='bg-black h-32 w-full flex justify-center items-center fixed top-0 z-10'>
+      <nav className='bg-black h-32 w-full flex justify-center items-center fixed top-0 z-10' style={{
+        background: "rgba(0,0,0)",
+        position: "fixed",
+        zIndex: 1,
+        top: "0",
+        right: "0",
+        bottom: "0",
+        left: "0",
+        opacity: scrollY > windowHeight - 132 ? "1" : "0",
+        transition: "all .3s",
+        visibility: scrollY > windowHeight - 132 ? "visible" : "hidden",
+      }}>
         <div className='flex items-center mt-3 mb-3'>
           <button className="relative inline-flex items-center justify-center mt-2 p-0.5 mb-2 me-2 overflow-hidden text-md font-light text-white rounded-lg group hover:bg-gradient-to-br from-gr-purple to-gr-pink">
             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black rounded-md">
@@ -93,7 +104,7 @@ export default function HomePage() {
             </span>
           </button>
         </div>
-      </nav> : <div></div>}
+      </nav>
       <div className=''>
         <ReactPlayer 
           playing
