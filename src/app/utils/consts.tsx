26b4fec5207353936ea3f5aa15de970b4732de1d
Bharat Kathi <bharat1031@gmail.com>
2023-12-24 08:24:09 -0800

    lint project

--- src/app/utils/consts.tsx
+++ src/app/utils/consts.tsx
@@ -1,5 +1,6 @@
 export const SOCIAL_LINKS = {
-    instagram: 'https://instagram.com/gauchoracingucsb',
-    twitter: 'https://twitter.com/bk1031_official',
-    linkedin: 'https://www.linkedin.com/company/gaucho-racing-at-uc-santa-barbara',
+  instagram: "https://instagram.com/gauchoracingucsb",
+  twitter: "https://twitter.com/bk1031_official",
+  linkedin:
+    "https://www.linkedin.com/company/gaucho-racing-at-uc-santa-barbara",
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,25 @@
-import type { Config } from 'tailwindcss'
-import colors from 'tailwindcss/colors'
+import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        'gr-pink': '#e105a3',
-        'gr-purple': '#8412fc',
+        "gr-pink": "#e105a3",
+        "gr-purple": "#8412fc",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
