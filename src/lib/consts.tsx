6098ba49391367299012a9c29a8adaef470dd732
Bharat Kathi <bharat1031@gmail.com>
2024-04-30 00:11:17 -0700

    update twitter url

--- src/lib/consts.tsx
+++ src/lib/consts.tsx
@@ -1,6 +1,6 @@
 export const SOCIAL_LINKS = {
   instagram: "https://instagram.com/gauchoracingucsb",
-  twitter: "https://twitter.com/bk1031_official",
+  twitter: "https://twitter.com/gaucho_racing",
   linkedin:
     "https://www.linkedin.com/company/gaucho-racing-at-uc-santa-barbara",
 };
