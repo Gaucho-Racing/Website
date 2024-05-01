8296bf13f47b2239e66a77f956812c43e186cfb3
Bharat Kathi <bharat1031@gmail.com>
2024-05-01 02:58:46 -0700

    add 2024 sponsor logos

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -9,6 +9,10 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import TeamCard from "@/components/team-card";
+import Image from "next/image";
+import Link from "next/link";
+import AutodeskIcon from "@/components/sponsors/autodesk";
+import TeslaIcon from "@/components/sponsors/tesla";
 
 export default function SponsorsPage() {
   return (
@@ -52,7 +56,11 @@ export default function SponsorsPage() {
       >
         <div className="bg-black bg-opacity-70 p-16">
           <div className="flex flex-col items-center justify-center text-white lg:mb-32 lg:ml-64 lg:mr-64 lg:mt-16">
-            <h1 className="p-16">Tier 1</h1>
+            <h1 className="p-16">Platinum</h1>
+            <div className="flex flex-col items-stretch lg:mt-16 lg:flex-row">
+              <AutodeskIcon className="h-32 w-64 bg-black text-white" />
+              <TeslaIcon className="h-16 w-32 bg-black text-white" />
+            </div>
           </div>
         </div>
       </div>
